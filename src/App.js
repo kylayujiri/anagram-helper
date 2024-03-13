e535fe9d292fb9d1d0cc6b89e40a898f0a61401e
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2024-03-13 14:42:54 -0400

    Add circular anagram format

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
-function LockButton({ i, value, isLocked ,handleLock }) {
-  let classStr = "lockBtn"
+function LockButton({ i, value, isLocked, handleLock, circular=false, circleX=0, circleY=0 }) {
+  let classStr = "lockBtn";
+  let styles = {};
+
+  if (circular) {
+    styles = {
+      transform: `translate(-50%, -50%) translate(${circleX}px, ${circleY}px)`
+    };
+    classStr += " circularTile";
+  }
 
   if (isLocked) {
     classStr += " locked";
@@ -11,13 +19,80 @@ function LockButton({ i, value, isLocked ,handleLock }) {
   }
 
   return (
-    <button key={i} className={classStr} onClick={() => handleLock(i)}>{value}</button>
+    <button key={i} className={classStr} style={styles} onClick={() => handleLock(i)}>{value}</button>
   );
+}
+
+function TileDisplay({ circular, word, locked, handleLock }) {
+
+  const displayRef = useRef(null);
+
+  const [displayHeight, setDisplayHeight] = useState(0);
+   useEffect(() => {
+      const height = displayRef.current.offsetHeight;
+      setDisplayHeight(height);
+   }, [displayRef]);
+
+  let lockButtons = [];
+
+  let radius = displayHeight / 2;
+
+  if (circular) {
+
+    for (let i = 0; i < word.length; i++) {
+      let circleX = getTileX(radius, word.length, i);
+      let circleY = getTileY(radius, word.length, i);
+
+      lockButtons.push(<LockButton i={i} value={word[i]} isLocked={locked[i]} handleLock={handleLock} circular={circular} circleX={circleX} circleY={circleY} />);
+    }
+
+    return (
+      <div class="tileDisplay" ref={displayRef}>
+        {lockButtons}
+      </div>
+    );
+
+  } else {
+
+    for (let i = 0; i < word.length; i++) {
+      lockButtons.push(<LockButton i={i} value={word[i]} isLocked={locked[i]} handleLock={handleLock} />);
+    }
+
+    return (
+      <div class="tileDisplay" ref={displayRef}>
+        <div class="wordDisplay">
+          <h1>{word}</h1>
+        </div>
+        <div class="lockButtons">
+          {lockButtons}
+        </div>
+      </div>
+    );
+
+  }
+}
+
+function getTileX(radius, numTiles, tileNum) {
+  if (tileNum >= numTiles) console.log("pointNum must be less than divisions");
+
+  let x = Math.sin(tileNum * 2 * Math.PI / numTiles) * radius;
+
+  return x;
+}
+
+function getTileY(radius, numTiles, tileNum) {
+  if (tileNum >= numTiles) console.log("tileNum must be less than numTiles");
+
+  // translation up is a negative y value instead of a positive value
+  let y = -Math.cos(tileNum * 2 * Math.PI / numTiles) * radius;
+
+  return y;
 }
 
 export default function Shuffler() {
   const [word, setWord] = useState([]);
   const [locked, setLocked] = useState([]);
+  const [circular, setCircular] = useState(false);
 
   const handleChange = (evt) => {
     setWord([...evt.target.value]);
@@ -60,22 +135,17 @@ export default function Shuffler() {
     setLocked(newLocked);
   }
 
-  let lockButtons = [];
-  for (let i = 0; i < word.length; i++) {
-    lockButtons.push(<LockButton i={i} value={word[i]} isLocked={locked[i]} handleLock={handleLock} />);
+  function handleSwitch() {
+    setCircular(!circular);
   }
 
   return (
     <div class="content">
-      <div class="word">
+      <button id="switchButton" onClick={handleSwitch}>SWITCH</button>
+      <div class="topBanner">
         <input type="text" id="wordInput" onChange={handleChange} />
       </div>
-      <div class="wordDisplay">
-        <h1>{word}</h1>
-      </div>
-      <div class="lockButtons">
-        {lockButtons}
-      </div>
+      <TileDisplay circular={circular} word={word} locked={locked} handleLock={handleLock} />
       <div class="shuffle">
         <button id="shuffleButton" onClick={handleShuffle}>
           SHUFFLE
